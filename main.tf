9378de32968d68147b61b71a9432e69facd81ba2
resaputra23 <resaputra23@gmail.com>
2026-08-20 04:07:22 +0000

    adding route table & internet gateway

--- main.tf
+++ main.tf
@@ -19,4 +19,28 @@ resource "aws_subnet" "public_testing0826" {
 
 }
 
+resource "aws_internet_gateway" "igw_testin0826" {
+  vpc_id = aws_vpc.testing0826.id
 
+  tags = {
+    Name = "igw_testing0826"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.testing0826.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_testin0826.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_testing0826.id
+  route_table_id = aws_route_table.public.id
+}
