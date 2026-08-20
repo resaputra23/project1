3dc312c810101df9b9db339717027f307b6568d0
resaputra23 <resaputra23@gmail.com>
2026-08-20 02:07:35 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "aws_vpc" "testing0826" {
+    cidr_block = var.vpc_cidr
+
+    tags = {
+        Name            = "testing project1"
+        Environtment    = "dev"
+        Project         = "testing automation"
+    }
+}
+
+resource "aws_subnet" "public_testing0826" {
+    vpc_id =  aws_vpc.testing0826.id 
+    cidr_block = var.public_subnet_cidr
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "subnet-public-testing-automation"
+    }
+
+}
+
+
---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+    region = var.aws_region
+}
+
