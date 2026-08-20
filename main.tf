79d704caa7795989426d5543eb33ac19c0891eb9
resaputra23 <resaputra23@gmail.com>
2026-08-20 04:36:58 +0000

    format terraform configuration

--- main.tf
+++ main.tf
@@ -51,18 +51,18 @@ resource "aws_security_group" "web" {
   vpc_id      = aws_vpc.testing0826.id
 
   ingress {
-    description = "HTTP" 
-    from_port  = 80
-    to_port    = 80
-    protocol   = "tcp"
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "HTTPS"
-    from_port  = 443
-    to_port    = 443
-    protocol   = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
