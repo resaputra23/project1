fb9872258232c1333675bab67649ec86969464f8
resaputra23 <resaputra23@gmail.com>
2026-08-20 05:18:44 +0000

    add nginx web server

--- main.tf
+++ main.tf
@@ -76,3 +76,44 @@ resource "aws_security_group" "web" {
     Name = "web-sg"
   }
 }
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.public_testing0826.id
+
+  vpc_security_group_ids = [
+    aws_security_group.web.id
+  ]
+
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              dnf uppdate -y
+              dnf install -y nginx
+              systemctl enable nginx
+              systemctl start nginx
+              EOF
+
+  tags = {
+    Name = "web-server"
+  }
+}
